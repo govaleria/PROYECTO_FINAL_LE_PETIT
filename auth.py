a737bf1bead5a587713b971a494bb481540b56f1
govaleria <valeriagarciaocampo05@gmail.com>
2026-03-29 19:18:10 -0500

    Update login

--- auth.py
+++ auth.py
@@ -4,9 +4,9 @@ oauth = OAuth()
 
 auth0 = oauth.register(
     'auth0',
-    client_id='B6yg1f2RoxUPZMTjhwaxYRATAAeJkSca',
-    client_secret='mlFjXEvoB734dZU3PSiRYpRnwAS5T2NakBU1z9OE-oC0pogGQu7f7mJhidWeQnCr', # <--- ¡Pégalo aquí directamente!
-    server_metadata_url='https://dev-fnxf4lcudrkaaiuh.us.auth0.com/.well-known/openid-configuration',
+    client_id='YgPJMTUuVnFdgnE5GGwMphb1MrCFAQcs',
+    client_secret='H1w2rtJ2Y5y7U-4HPvhiKy-t_1AOZxvRsumQCCYolrBqPtk065xV5NrZQz8O8RVT', # <--- ¡Pégalo aquí directamente!
+    server_metadata_url='https://dev-a7pcxol1levay1br.us.auth0.com/.well-known/openid-configuration',
     client_kwargs={
         'scope': 'openid profile email',
         'token_endpoint_auth_method': 'client_secret_post', # Esto es vital para evitar el Unauthorized
